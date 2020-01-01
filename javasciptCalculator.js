6512f3cccad14a1f9d06a9d9c28cfed6f0234e9c
Min Hyeok Seol <dat.sci.seol@gmail.com>
2020-01-01 21:21:49 +0900

    The entire file for calculator.js

---
+++ javasciptCalculator.js
@@ -0,0 +1,24 @@
+// Learning Javascript grammar
+// Calculator using JS
+const calculator = {
+    plus : function(a, b){
+      return a + b;
+    },
+    minus : function(a, b){
+      return a - b;
+    },
+  
+    multiply : function(a, b){
+      return a * b;
+    },
+  
+    divide : function(a, b){
+      return a / b;
+    },
+  
+    square_root : function(a){
+      return a ** 0.5;
+    }
+    }
+  
+  
